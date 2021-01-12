5a46f9797d8be1292ab2d52f7970e699d794d0b4
Aman Tawakley <tawakley.aman@gmail.com>
2021-01-12 17:38:16 -0500

    Simple express app setup and db connect

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
 
+const app = express();
+mongoose.connect(
+	process.env.MONGO_URI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log("Connected to db!")
+);
 
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/auth", require("./routes/auth"));
+
+app.get("/", (req, res) => {
+	res.json({ msg: "Hello" });
+});
+
+const port = process.env.PORT || 1234;
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
