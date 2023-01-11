8ece308830e3e076ecf371fc7dc389f94e901272
Aman Tawakley <tawakley.aman@gmail.com>
2023-01-10 20:47:46 -0500

    Auth validation helper in ES6

--- src/helper/authValidation.js
+++ src/helper/authValidation.js
@@ -1,21 +1,19 @@
-const Joi = require("joi");
+import Joi from "joi"
 
-const registerValidation = (data) => {
+export function registerValidation(data) {
 	const validUser = Joi.object({
 		name: Joi.string().min(3).max(255).required(),
 		username: Joi.string().min(5).max(255).required(),
 		email: Joi.string().email().required(),
 		password: Joi.string().min(8).max(1024).required(),
-	});
-	return validUser.validate(data);
-};
+	})
+	return validUser.validate(data)
+}
 
-const loginValidation = (data) => {
+export function loginValidation(data) {
 	const validUser = Joi.object({
 		username: Joi.string().min(5).max(255).required(),
 		password: Joi.string().min(8).max(1024).required(),
-	});
-	return validUser.validate(data);
-};
-
-module.exports = { registerValidation, loginValidation };
+	})
+	return validUser.validate(data)
+}
